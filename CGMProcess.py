cfb9a4859da2b314a02748f09f548fd855f5f9f3
AKNeyugn <anhkhoi.nguyenhoang@gmail.com>
2019-08-02 14:52:02 -0500

    added process_half_filter function to CGMProcess.py

--- CGMProcess.py
+++ CGMProcess.py
@@ -21,6 +21,7 @@ output_folder = "CGM"
 first_half = "2019-05-15_split-lfc-p-values-strains-A-M.csv"
 second_half = "2019-05-15_split-lfc-p-values-strains-N-Z.csv"
 output_full = "Full_CGM"
+column_names = ["z_score", "p_value"]
 
 def main():
     start = datetime.datetime.now()
@@ -32,20 +33,31 @@ def main():
 
     filter_run = sys.argv[1]
 
-    if filter_run.upper() == "FALSE":
+    if filter_run.upper() == "TRUE":
+        z_score_cutoff = sys.argv[2]
+        p_value_cutoff = sys.argv[3]
+
+        # Create filtered matrix
+        output_name = output_full + "_" + str(z_score_cutoff) + "_" + str(p_value_cutoff) + ".csv"
+        # Create DataFrame for CGM halves
+        df_first = process_half_filter(input_first, z_score_cutoff, p_value_cutoff)
+        df_second = process_half_filter(input_second, z_score_cutoff, p_value_cutoff)
+        # Combine CGM halves
+        combine_halves(df_first, df_second, output_name)
+    else:
         # Create Z-score matrix
         output_name = output_full + "_Z_score.csv"
         # Create DataFrame for CGM halves
-        df_first = process_half(input_first, "z_score")
-        df_second = process_half(input_second, "z_score")
+        df_first = process_half(input_first, column_names[0])
+        df_second = process_half(input_second, column_names[0])
         # Combine CGM halves
         combine_halves(df_first, df_second, output_name)
 
         # Create Z-score matrix
         output_name = output_full + "_P_value.csv"        
         # Create DataFrame for CGM halves
-        df_first = process_half(input_first, "p_value")
-        df_second = process_half(input_second, "p_value")
+        df_first = process_half(input_first, column_names[1])
+        df_second = process_half(input_second, column_names[1])
         # Combine CGM halves
         combine_halves(df_first, df_second, output_name)
 
@@ -90,6 +102,60 @@ def process_half(input_name, cell_value):
                 cgm_cmps.append(cmp_name)
             # Get cell values
             score = row[cell_value]
+            cgm_data[strain].append(score)
+            sys.stdout.write("Processed %d mutants \r" % (num_cmp))
+            sys.stdout.flush()
+            num_cmp += 1
+
+    data_df = pd.DataFrame.from_dict(cgm_data)
+    indices = dict(zip(list(range(len(cgm_cmps))),cgm_cmps))
+    data_df.rename(indices, axis="index", inplace=True)
+
+    sys.stdout.write("Done!\n")
+    sys.stdout.write("\n")
+    return data_df
+
+def process_half_filter(input_name, z_score_cutoff, p_value_cutoff):
+    '''
+    Read chemogenomic data file and create DataFrame with binary cell values;
+    1 if input values pass z_score AND p_value cutoffs, 0 otherwise 
+
+    Args:
+        input_name (string): name of chemogenomic data file
+        z_score_cutoff (float): z_score cutoff
+        p_value_cutoff (float): p_value cutoff
+    '''
+    sys.stdout.write("Processing " + input_name + "...\n")
+    df = pd.read_csv(input_name)
+    cgm_data = {}
+    cgm_cmps = []
+
+    for index, row in df.iterrows():
+        strain = row["strain"]
+        if strain not in cgm_data.keys():
+            # break to shorten output data during testing
+            #if len(cgm_data.keys()) == 2:
+                #break
+            cgm_data[strain] = []
+            num_cmp = 1
+            sys.stdout.write("\n")
+            sys.stdout.write("Processing " + str(strain) + "...\n")
+
+        cmp_stem = row["compound_stem"]
+        if str(cmp_stem) != "nan":
+            # Get compound name
+            compound = row["compound"]
+            conct = row ["concentration"]
+            cmp_name = compound + "-" + str(conct)
+            if cmp_name not in cgm_cmps:
+                cgm_cmps.append(cmp_name)
+            # Get cell values
+            z_score = row[column_names[0]]
+            p_value = row[column_names[1]]
+            if float(z_score) <= z_score_cutoff and float(p_value) <= p_value_cutoff:
+                score = "1"
+            else:
+                score = "0"
             cgm_data[strain].append(score)
             sys.stdout.write("Processed %d mutants \r" % (num_cmp))
             sys.stdout.flush()
